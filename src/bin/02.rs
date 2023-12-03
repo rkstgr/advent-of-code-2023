cf3d16214e6bef4523a9688158170a5d866973e0
Erik Steiger <rkstgr@gmail.com>
2023-12-03 15:51:13 +0100

    chore/day02: Reuse code between both tasks

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -8,54 +8,6 @@ enum Color {
     Blue,
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let max_cubes = HashMap::from([(Color::Red, 12), (Color::Green, 13), (Color::Blue, 14)]);
-
-    let mut sum_of_ids = 0;
-
-    for line in input.lines() {
-        let parts: Vec<&str> = line.split(": ").collect();
-        let game_id: u32 = parts[0].replace("Game ", "").parse().unwrap();
-        let rounds = parts[1].split(';');
-
-        let mut possible = true;
-        for round in rounds {
-            let cubes: HashMap<Color, u32> = round
-                .split(',')
-                .map(|c| c.trim())
-                .map(|c| {
-                    let parts: Vec<&str> = c.split_whitespace().collect();
-                    let number = parts[0].parse::<u32>().unwrap();
-                    let color = match parts[1] {
-                        "red" => Color::Red,
-                        "green" => Color::Green,
-                        "blue" => Color::Blue,
-                        _ => panic!("Unknown color"),
-                    };
-                    (color, number)
-                })
-                .collect();
-
-            for (&color, &number) in cubes.iter() {
-                if number > *max_cubes.get(&color).unwrap() {
-                    possible = false;
-                    break;
-                }
-            }
-
-            if !possible {
-                break;
-            }
-        }
-
-        if possible {
-            sum_of_ids += game_id;
-        }
-    }
-
-    Some(sum_of_ids)
-}
-
 fn parse_cubes_info(info: &str) -> HashMap<Color, u32> {
     info.split(',')
         .map(|c| c.trim())
@@ -79,6 +31,37 @@ fn calculate_power(min_cubes: &HashMap<Color, u32>) -> u32 {
         * min_cubes.get(&Color::Blue).unwrap_or(&0)
 }
 
+// Updated `part_one` to use the `parse_cubes_info` helper function
+pub fn part_one(input: &str) -> Option<u32> {
+    let max_cubes = HashMap::from([(Color::Red, 12), (Color::Green, 13), (Color::Blue, 14)]);
+    let mut sum_of_ids = 0;
+
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(": ").collect();
+        let game_id: u32 = parts[0].replace("Game ", "").parse().unwrap();
+        let rounds = parts[1].split(';');
+
+        let mut possible = true;
+        for round in rounds {
+            let cubes = parse_cubes_info(round);
+
+            if cubes
+                .iter()
+                .any(|(&color, &number)| number > *max_cubes.get(&color).unwrap())
+            {
+                possible = false;
+                break;
+            }
+        }
+
+        if possible {
+            sum_of_ids += game_id;
+        }
+    }
+
+    Some(sum_of_ids)
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
     let mut powers_sum = 0;
 
@@ -86,7 +69,6 @@ pub fn part_two(input: &str) -> Option<u32> {
         let parts: Vec<&str> = line.split(": ").collect();
         let rounds = parts[1].split(';');
 
-        // Initialize the minimum cubes with zero
         let mut min_cubes: HashMap<Color, u32> = HashMap::new();
 
         for round in rounds {
@@ -94,9 +76,7 @@ pub fn part_two(input: &str) -> Option<u32> {
 
             for (&color, &number) in cubes.iter() {
                 let entry = min_cubes.entry(color).or_insert(0);
-                if number > *entry {
-                    *entry = number;
-                }
+                *entry = number.max(*entry);
             }
         }
 
